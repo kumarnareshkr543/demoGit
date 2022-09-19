19ceac4b7cf7768eea29c9727a715c76a3300e69
naresh003kr <naresh.003kr@gmail.com>
2022-09-19 11:42:34 +0530

    first

---
+++ src/main/java/com/company/demogit/DemoGitApplication.java
@@ -0,0 +1,15 @@
+package com.company.demogit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGitApplication.class, args);
+        System.out.println("----------");
+    }
+
+}
+
